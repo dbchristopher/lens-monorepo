6869ea6005dedfa601e30e062512f861885a5f20
Daniel Christopher <dbchristopher@gmail.com>
2019-09-11 12:25:20 -0700

    typescript config

--- .docz/doczrc.js
+++ .docz/doczrc.js
@@ -1,6 +1,6 @@
 export default {
+	typescript: true,
 	title: 'Looker Lens Styleguide',
 	menu: ['Getting Started', 'Components'],
-	typescript: true,
 	files: 'packages/styleguide/**/*.{md,markdown,mdx}'
 };
--- packages/components/src/Link/Link.tsx
+++ packages/components/src/Link/Link.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 interface LinkProps {
   children: React.ReactNode;
   id: string;
+  href: string;
 }
 
 const Link = styled.a``;
