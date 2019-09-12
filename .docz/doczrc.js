d75f73b0fbeafaa7869e154bec5483e36f144d5b
Daniel Christopher <dbchristopher@gmail.com>
2019-09-12 10:37:32 -0700

    ignore docz output

--- .docz/doczrc.js
+++
@@ -1,6 +0,0 @@
-export default {
-	typescript: true,
-	title: 'Looker Lens Styleguide',
-	menu: ['Getting Started', 'Design Tokens', 'Icons', 'Components'],
-	files: 'packages/styleguide/**/*.{md,markdown,mdx}',
-}
